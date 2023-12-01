64dd2d6645108020eabb21d83dd71c3061a7fbc3
Debojit Santra <debojitsantra48@gmail.com>
2023-12-02 01:07:51 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+image_folder = "/storage/emulated/0/Documents/Generated"
+json_file = "images.json"
+
+image_paths = [f"{image_folder}/{image}" for image in os.listdir(image_folder) if image.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+data = {"images": image_paths}
+
+with open(json_file, "w") as json_file:
+    json.dump(data, json_file, indent=2)
+
+print(f"JSON file '{json_file}' generated successfully.")
