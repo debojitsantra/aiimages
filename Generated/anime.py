a354e380227033c7212d18ce415ef8dc8107d9ba
Debojit Santra <debojitsantra48@gmail.com>
2024-02-14 20:20:30 +0530

    Add files via upload

---
+++ Generated/anime.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+image_folder = "Anime"
+json_file = "anime.json"
+
+image_paths = [f"/Generated/{image_folder}/{image}" for image in os.listdir(image_folder) if image.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+data = {"images": image_paths}
+
+with open(json_file, "w") as json_file:
+    json.dump(data, json_file, indent=2)
+
+print(f"JSON file '{json_file}' generated successfully.")
